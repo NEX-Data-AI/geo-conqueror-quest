9af564b94961c4202d44ea7599a3c9de9742eff2
Donnie Arbeau <161102449+NEX-Data-AI@users.noreply.github.com>
2025-11-03 10:23:44 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Map from "./pages/Map";
 import GIS from "./pages/GIS";
@@ -13,9 +14,9 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
+        <Sonner />
+        <Toaster />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/map" element={<Map />} />
