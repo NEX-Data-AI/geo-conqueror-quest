932816bc2c57edb31f83a52fc398b21d7803e39a
Donnie Arbeau <161102449+NEX-Data-AI@users.noreply.github.com>
2025-11-03 13:34:53 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Index from "@/pages/Index";
-import Map from "@/pages/Map";
-import GIS from "@/pages/GIS";
-import NotFound from "@/pages/NotFound";
+import Index from "./pages/Index";
+import Map from "./pages/Map";
+import GIS from "./pages/GIS";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
